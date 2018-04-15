f82ca5f66839544a296fb1fed4b68cc76e8da608
gperez8 <gregory.facyt@gmail.com>
2018-04-14 23:37:53 -0400

    se quitaron console.log

--- src/actions/actionsCreator.js
+++ src/actions/actionsCreator.js
@@ -1,12 +1,4 @@
 import { INCREMENT, DECREMENT } from './actions';
 
-export const increment = () => {
-	console.log('PASO POR EL ACTION_CREATOR_INCREMENT');
-	return { type:  INCREMENT, text: 'SE INCREMENTO EL CONTADOR'}
-
-};
-
-export const decrement = () => {
-	console.log('PASO POR EL ACTION_CREATOR_DECREMENT');
-	return { type:  DECREMENT, text: 'SE DECREMENTO EL CONTADOR'}
-};
+export const increment = () => ({type:  INCREMENT, text: 'SE INCREMENTO EL CONTADOR'});
+export const decrement = () => ({type:  DECREMENT, text: 'SE DECREMENTO EL CONTADOR'});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,10 @@ import { INCREMENT, DECREMENT } from '../actions/actions';
 export default (state = 0, action) => {
 	switch (action.type) {
 		case INCREMENT:
-			console.log('PASO POR EL REDUCER_INCREMENT');
 			return state + 1;
-		case DECREMENT: 
-			console.log('PASO POR EL REDUCER_DECREMENT');
+		case DECREMENT:
 			return state - 1;
-		default: 
-			console.log('PASO POR EL REDUCER_DEFAULT');
+		default:
 			return state;
 	}
 }
