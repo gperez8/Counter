82f746cbd409bd70c347d5bafd29f435d4bae032
gperez8 <gregory.facyt@gmail.com>
2018-04-15 23:30:17 -0400

    Se agrego redux devtool

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import Counter from './components/Counter';
 import counter from './reducers';
 
-const store = createStore(counter);
+const store = createStore(counter, composeWithDevTools());
 const rootID = document.getElementById("root");
 
 render(
